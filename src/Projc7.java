f4bbe34364aae5af995fd54015c355764264cf83
poletaiev@gmail.com <slayers99>
2017-12-26 21:00:19 +0200

    Initial commit

---
+++ src/Projc7.java
@@ -0,0 +1,167 @@
+/**
+ * Created by java on 26.12.2017.
+ */
+public class Projc7 {
+
+    public static void main(String[] args) {
+//        int a=15;
+//        long b=12;
+//        float c =22.5F;
+//        double h=22.5;
+//        long ab=12L;
+//        System.out.println(b);
+
+  //      int a=12;
+
+  //      int b=15;
+
+    //    int sum=a+b;
+
+//        int sumForObject;
+  //      int c=b%a;
+//        long b=8;
+//
+   //    long sum= b+a;
+//        double d=22/50;
+//        float f=1234.56f;
+//        double result = (float) (d+f);
+
+
+  //      boolean flag1=false;
+  //      boolean flag2=true;
+
+//        System.out.println(flag1);
+//        System.out.println(flag2);
+
+
+ //       String str="first str";//колличество символов =9
+        String strOne="first";
+        String strTwo="two";
+
+ //       str.length();
+ //       str.contains("f");
+ //       System.out.println(str.isEmpty());
+
+ //       System.out.println(str.length());
+
+  //      System.out.println(str.length()==9);
+  //      System.out.println(str.length()==8);
+//        System.out.println(str.contains("p"));//ищет есть пи такая буква
+  //      System.out.println(str.length()<9);
+
+
+//        int a=5;
+//        double c=45;
+
+//        String str="first str";
+//        String str2="first str";
+//
+//        String strNew=new String("fox");
+//        String strNew2=new String("fox");
+//
+//
+//        System.out.println(str==str2);
+//        System.out.println(strNew==strNew2);
+
+//        String str="first str";
+//        String strNew=str;
+  //      String str3=str.substring(0,3);//обрезает букві с 0до3 fir
+//        String str1="1";
+//        String str2="2";
+// //       System.out.println(strNew);
+//   //     System.out.println(str3);
+//        System.out.println(str1+str2);
+//        System.out.println(str1.length()+str2.length());
+//        int a=15;
+//        String aStr=String.valueOf(a);
+//
+//        int one= Integer.parseInt(str1);
+//
+//        float flt= Float.parseFloat(str1);
+//
+//        float f= Float.parseFloat(str1);
+
+        String str1="1";
+        String str2="2";
+//        int a=Integer.parseInt(str1);
+//        int b=Integer.parseInt(str2);
+//
+//        int sum=a+b;
+//        System.out.println(sum);
+
+  //      String str3="www34";//нужно получить 3+4
+ //       int ab=Integer.parseInt(str3);
+ //       System.out.println(ab);
+
+  //      String str4= str3.substring(3);//вывел до конца строки 34
+  //      System.out.println(str4);
+
+   //     int ab=Integer.parseInt(str3);
+
+
+
+ //        int sum2=Integer.parseInt(str1)+Integer.parseInt(str2);
+ //       System.out.println(sum2);
+
+        //massive
+
+ //       int[] mass=new int[12];
+
+//        int[] mass2={1,2,3,4,5};
+//        System.out.println(mass2.length);
+//        System.out.println(mass2[0]);//віводи номер єелемента
+//        System.out.println(mass2.toString());//адрес памяти
+
+
+//        int mass[]=new int[10];
+//        mass[0]=5;
+//        mass[1]=2;
+//        mass[2]=5;
+//        mass[3]=3;
+//        mass[4]=5;
+//        mass[9]=1;
+
+//        for(int i=0;i<mass.length;i++){
+//            System.out.print(mass[i]+"");
+  //      }
+
+//        for(int mas:mass){
+//            System.out.println(mas);
+//        }
+//        String str="ajgwjhgwfg";
+//
+    //    int mass2[]=str;
+
+
+        //cicl for each
+
+//for(int i=0;i<mass.length;i++){
+//    if(mass[i]==5){
+//        float f=i;
+//        System.out.println(f);
+//    }
+//    System.out.println(mass[i]+"  ");
+//
+//}
+//        for(int mas:mass){//mas new peremennaya
+//           System.out.print(mas+" ");
+//       }
+//        for(int i:mass){
+//            if(i==5){
+//                float f=i;
+//                System.out.println(f);
+//            }
+//        }
+
+       String  mass[]={"One","Lola,"};
+        for(String i:mass){
+            System.out.println(i);
+        }
+
+
+
+
+    }
+
+
+}
